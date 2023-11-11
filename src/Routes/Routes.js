ea19ce734032062963c517b64751683cbb6eb035
tohirul <tohirul.islam164@gmail.com>
2023-11-11 14:06:00 +0600

    twak

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
-import Blog from "../Pages/Blog/Blog";
 import CheckOut from "../Pages/CheckOut/CheckOut";
 import Course from "../Pages/Course/Course";
 import Courses from "../Pages/Courses/Courses";
@@ -11,50 +10,59 @@ import Page404 from "../Shared/Page404/Page404";
 import Registration from "../Shared/Registration/Registration";
 
 export const routes = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main></Main>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>,
-            },
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
 
-            {
-                path: '/login',
-                element: <Login></Login>,
-            },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
 
-            {
-                path: '/register',
-                element: <Registration></Registration>,
-            },
-            // {
-            //     path: '/blog',
-            //     element: <Blog></Blog>,
-            // },
+      {
+        path: "/register",
+        element: <Registration></Registration>,
+      },
+      // {
+      //     path: '/blog',
+      //     element: <Blog></Blog>,
+      // },
 
-            {
-                path: '/course/:id',
-                element: <Course></Course>,
-                loader: ({ params }) => fetch(`https://platinam-tutorial-server.vercel.app/course/${params.id}`)
-            },
-            {
-                path: '/courses',
-                element: <Courses></Courses>,
-                loader: () => fetch('https://platinam-tutorial-server.vercel.app/courses')
-            },
-            {
-                path: '/checkout/:id',
-                element: <PrivateRoute><CheckOut></CheckOut></PrivateRoute>,
-                loader: ({ params }) => fetch(`https://platinam-tutorial-server.vercel.app/course/${params.id}`)
-            },
-
-        ]
-    },
-    {
-        path: '*',
-        element: <Page404></Page404>,
-    }
-
+      {
+        path: "/course/:id",
+        element: <Course></Course>,
+        loader: ({ params }) =>
+          fetch(
+            `https://platinam-tutorial-server.vercel.app/course/${params.id}`
+          ),
+      },
+      {
+        path: "/courses",
+        element: <Courses></Courses>,
+        loader: () =>
+          fetch("https://platinam-tutorial-server.vercel.app/courses"),
+      },
+      {
+        path: "/checkout/:id",
+        element: (
+          <PrivateRoute>
+            <CheckOut></CheckOut>
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(
+            `https://platinam-tutorial-server.vercel.app/course/${params.id}`
+          ),
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <Page404></Page404>,
+  },
 ]);
